a0a9caf2edae448830d5646dbaca563c3e48e2a0
TimeChaser <1872101334@qq.com>
2021-11-27 00:06:10 +0800

    Fixed the error of project pop-ups appear multiple times.

--- src/main/java/top/isopen/plugin/listener/ProjectListener.java
+++ src/main/java/top/isopen/plugin/listener/ProjectListener.java
@@ -5,15 +5,23 @@ import com.intellij.openapi.project.ProjectManagerListener;
 import org.jetbrains.annotations.NotNull;
 import top.isopen.plugin.dialog.SoupDialog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author TimeChaser
  * @since 2021/11/25 11:10
  */
 public class ProjectListener implements ProjectManagerListener {
 
+    private static final List<Project> PROJECT_LIST = new ArrayList<>();
+
     @Override
     public void projectOpened(@NotNull Project project) {
-        SoupDialog soupDialog = new SoupDialog();
-        soupDialog.show();
+        if (!PROJECT_LIST.contains(project)) {
+            SoupDialog soupDialog = new SoupDialog();
+            soupDialog.show();
+            PROJECT_LIST.add(project);
+        }
     }
 }
